f743158ab7a4a7404c6e180a52878cdb1ba68f24
Asif <asif.mahmood@iub.edu.bd>
2024-10-20 09:37:46 +0600

    Adde 'age' field in the constructor.

--- src/main/java/oop/githubdemo/User.java
+++ src/main/java/oop/githubdemo/User.java
@@ -4,8 +4,9 @@ public class User {
     String username, password;
     int age;
 
-    public User(String username, String password) {
+    public User(String username, String password, int age) {
         this.username = username;
         this.password = password;
+        this.age = age;
     }
 }
