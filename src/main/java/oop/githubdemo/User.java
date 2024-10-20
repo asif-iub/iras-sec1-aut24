88f577ba65084725ec4a6ca3119aca9ff827eaf4
Asif <asif.mahmood@iub.edu.bd>
2024-10-20 09:34:52 +0600

    Added constructor to the User model class

--- src/main/java/oop/githubdemo/User.java
+++ src/main/java/oop/githubdemo/User.java
@@ -2,4 +2,9 @@ package oop.githubdemo;
 
 public class User {
     String username, password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
