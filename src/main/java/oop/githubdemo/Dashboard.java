c9f274db1086748904b0e33572557b0f7a9b73cd
Asif <asif.mahmood@iub.edu.bd>
2024-10-20 10:53:57 +0600

    Added scene switching after log in.

---
+++ src/main/java/oop/githubdemo/Dashboard.java
@@ -0,0 +1,24 @@
+package oop.githubdemo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Dashboard {
+    @FXML
+    protected void onLogOutButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+    }
+}
--- src/main/java/oop/githubdemo/HelloController.java
+++ src/main/java/oop/githubdemo/HelloController.java
@@ -1,9 +1,17 @@
 package oop.githubdemo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -16,7 +24,25 @@ public class HelloController {
     private PasswordField passwordInput;
 
     @FXML
-    protected void onSignInButtonClick() {
+    protected void onSignInButtonClick(ActionEvent event) throws IOException {
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
 
+        if (username.equals("asif") && password.equals("1234")) {
+            msgLabel.setText("Log in successful!");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//            Stage stage = new Stage();
+            stage.setScene(scene);
+//            stage.show();
+        }
+        else {
+            msgLabel.setText(("Log in failed!"));
+        }
     }
 }
