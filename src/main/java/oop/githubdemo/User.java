136dd9d438721bbf9d2375800f7f8425b519ce8d
Asif <asif.mahmood@iub.edu.bd>
2024-10-29 08:52:06 +0600

    Added getter and setter in the User class. Also added Back and Log Out button controller codes.

--- src/main/java/oop/githubdemo/User.java
+++ src/main/java/oop/githubdemo/User.java
@@ -9,4 +9,28 @@ public class User {
         this.password = password;
         this.age = age;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/oop/githubdemo/UserManagementController.java
+++ src/main/java/oop/githubdemo/UserManagementController.java
@@ -2,10 +2,17 @@ package oop.githubdemo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserManagementController {
 
@@ -43,13 +50,25 @@ public class UserManagementController {
     }
 
     @FXML
-    void onBackButtonClick(ActionEvent event) {
+    void onBackButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+        Parent root = fxmlLoader.load();
 
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
     }
 
     @FXML
-    void onLogOutButtonClick(ActionEvent event) {
+    void onLogOutButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
 
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
     }
 
 }
